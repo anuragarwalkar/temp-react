cd71e5b1cefb7f5fd68e351b2d2f2a12ded3282e
Anurag Arwalkar <anurag.arwalkar@globant.com>
2022-07-06 13:18:44 +0530

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
+const Work = React.lazy(() => import("./Nested"));
 
 function About() {
-  return <h2>About</h2>;
+  return (
+    <Suspense fallback={<span>Loading Inner......</span>}>
+      <Switch>
+        <Route path="/about" exact render={() => <div><h1>Helllllo</h1> <Link to="/about/work">Or work</Link></div> } />
+        <Route path="/about/work" component={Work} />
+      </Switch>
+    </Suspense>
+  );
 }
 
 export default About;
---
+++ src/Components/Nested.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Nested () {
+    return <div><h1>Hello sdsd</h1> <Link to="/about">Home</Link></div>;
+}
+
+export default Nested;
